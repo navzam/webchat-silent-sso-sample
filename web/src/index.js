8dcfe973a9e77f024fa3d59090d86b3f5d4536e1
Nafis Zaman <nazam@microsoft.com>
2020-01-23 15:23:53 -0800

    Initial commit

---
+++ web/src/index.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+
+// Setting default environment variables
+process.env = {
+    PORT: '5000',
+    STATIC_FILES: 'public',
+    OAUTH_METADATA_ENDPOINT: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
+    ...process.env,
+};
+
+const { join } = require('path');
+const restify = require('restify');
+const passport = require('passport');
+const { BearerStrategy } = require('passport-azure-ad');
+
+const server = restify.createServer();
+const { STATIC_FILES, PORT, OAUTH_CLIENT_ID, TENANT_ID, OAUTH_METADATA_ENDPOINT } = process.env;
+
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+
+// Set up the passport authentication strategy
+var bearerStrategy = new BearerStrategy({
+    identityMetadata: OAUTH_METADATA_ENDPOINT,
+    clientID: OAUTH_CLIENT_ID,
+    validateIssuer: true,
+    issuer: `https://login.microsoftonline.com/${TENANT_ID}/v2.0`,
+    loggingLevel: 'info',
+    loggingNoPII: false,
+}, (token, done) => {
+    console.info(`the token retrieved is ${token}`);
+    const user = {
+        id: token.sub,
+        name: token.name,
+        upn: token.preferred_name,
+    };
+    return done(null, user, token);
+});
+passport.use(bearerStrategy);
+
+// Register route for getting a DirectLine token
+// Protected using Passport, which will validate the incoming ID token
+server.post('/api/directline/token', passport.authenticate('oauth-bearer', { session: false }), require('./routes/directLine/token'));
+
+// We will use the REST API server to serve static web content to simplify demployment for demonstration purposes
+if (STATIC_FILES) {
+    server.get('/**/*', restify.plugins.serveStatic({
+        default: 'index.html',
+        directory: join(__dirname, '..', STATIC_FILES),
+    }));
+}
+
+server.listen(PORT, () => {
+    if (STATIC_FILES) console.log(`Will serve static content from ${STATIC_FILES}`);
+
+    console.log(`REST API server is listening to port ${PORT}`);
+});
---
+++ web/src/routes/directLine/token.js
@@ -0,0 +1,31 @@
+const fetchJSON = require('../utils/fetchJSON');
+
+async function generateDirectLineToken(secret, userId) {
+    console.log(`Generating DL token for ${userId} (${secret})`);
+    const directLineResponse = await fetchJSON('https://directline.botframework.com/v3/directline/tokens/generate', {
+        headers: {
+            Authorization: `Bearer ${secret}`,
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            User: { Id: userId },
+        }),
+    });
+
+    return directLineResponse.token;
+}
+
+const { DIRECT_LINE_SECRET } = process.env;
+
+// GET /api/directline/token
+// Generates a new Direct Line token
+module.exports = async (req, res, next) => {
+    // Incoming ID token has already been validated by Passport
+    // Get user ID from resulting user object
+    const userId = req.user.id;
+
+    console.log(`Getting DirectLine token for user ID ${userId}`);
+    const directLineToken = await generateDirectLineToken(DIRECT_LINE_SECRET, userId);
+    res.json({ token: directLineToken });
+};
